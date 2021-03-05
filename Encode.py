7d0608045b1d81bc8121e90561cc9e61e97d329d
dinhquangomg <dinhquangomg@gmail.com>
2021-03-05 17:59:59 +0700

    create GUI by tkinter

--- Encode.py
+++ Encode.py
@@ -1,1 +1,16 @@
-print("Nguyen Dinh Quang")
+from tkinter import *
+
+window=Tk()
+window.title("Encode")
+window.geometry("400x500")
+
+label_encode=Label(
+    master=window,
+    text="ENCODE",
+    foreground="#f64c72",
+    font=("arial",16,"bold")
+)
+label_encode.place(x=160, y=20)
+
+
+window.mainloop()
