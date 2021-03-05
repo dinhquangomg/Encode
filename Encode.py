63b455d78b9ac094de26b64740518663be847b6e
dinhquangomg <dinhquangomg@gmail.com>
2021-03-05 19:12:07 +0700

    add code to complete GUI

--- Encode.py
+++ Encode.py
@@ -2,15 +2,36 @@ from tkinter import *
 
 window=Tk()
 window.title("Encode")
-window.geometry("400x500")
+window.geometry("400x450")
 
 label_encode=Label(
     master=window,
     text="ENCODE",
     foreground="#f64c72",
-    font=("arial",16,"bold")
+    font=("arial",24,"bold")
 )
-label_encode.place(x=160, y=20)
+text_input=Text(
+    master=window,
+    height=8,
+    width=40,
+    background="#659dbd"
+)
 
+text_output=Text(
+    master=window,
+    height=8,
+    width=40,
+    background="#8ee4af"
+)
+
+button_encode=Button(
+    window,
+    text="Encode"
+)
+
+label_encode.pack(pady=10)
+text_input.pack(pady=10)
+button_encode.place(x=100,y=220)
+text_output.pack(pady=50)
 
 window.mainloop()
